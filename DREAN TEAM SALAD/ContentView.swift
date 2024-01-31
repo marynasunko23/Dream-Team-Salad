38767ba59a60422e11b26639486f169ebb220f6d
msunko <maryna.sunko@gmail.com>
2024-01-31 12:05:23 -0500

    Initial Commit

---
+++ DREAN TEAM SALAD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DREAN TEAM SALAD
+//
+//  Created by Maryna Sunko on 1/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DREAN TEAM SALAD/DREAN_TEAM_SALADApp.swift
@@ -0,0 +1,17 @@
+//
+//  DREAN_TEAM_SALADApp.swift
+//  DREAN TEAM SALAD
+//
+//  Created by Maryna Sunko on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct DREAN_TEAM_SALADApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
