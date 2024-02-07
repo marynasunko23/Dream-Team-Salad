3a99db936e706badaf6fd0242c35786f876e7717
Dewayne Williams <jujubean3157@gmail.com>
2024-02-07 11:59:04 -0500

    x code project file

--- DREAN TEAM SALAD/salad.swift
+++ DREAN TEAM SALAD/salad.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+
+
+
+
+
+
+
+struct Salad {
+    let Name : String
+    let Ingredents : String
+    let Picture : String
+
+    
+    static let Broccoli =  Salad (Name : "VBroccoli salad", Ingredents : "broccoli, bacon, green onions, red grapes  red onion", Picture : "Broccoli Salad")
+    
+    static let Tuna = Salad (Name : "Tuna Salad", Ingredents : "Tuna Fish, Boilled  eggs,, , Mayo, Relish", Picture : "Tuna Salad" )
+    
+    static let Potato = Salad (Name : "Potato Salad", Ingredents : "Baked or Boilled and peeled Potatos, Hard  boilled eggs, Mustard, Mayo, Relish, Peperika", Picture :"Potato Salad" )
+    
+    static let Farmers = Salad ( Name : "Farmers Salad", Ingredents : "Iceburg lettice, Tomatos, Cucomber, Boilled egg, Ham, Cheese, Onion,", Picture : "Farmers")
+    
+
+    
+    static let taco = Salad (Name : "Taco Salad", Ingredents : "Taco meat, tomato, lettace,Cheese, Sour Cream", Picture : "Taco Salad")
+    
+    static let Cobb = Salad ( Name : "Cobb Salad Salad", Ingredents : "Mesclun greens, Bacon, Haerd Boiled egg, Tomato, Salt, Pepper,Avocado", Picture :"Cobb Salad" )
+    
+    static let farmer = Salad (Name : "Faermers Salad", Ingredents : "lettace, Cottage cheese, Tomato,Cucomber, Greeen Pepper, Suger snap peas", Picture : "Farmer" )
+    
+    static let Fruit = Salad (Name : "Fruit Salad", Ingredents : "Watermellon, Pinappple, str, ,Honey Dew,Canalope", Picture : "Fruit Salad")
+    
+    static let Greek = Salad (Name : "Greek Salad", Ingredents : "Lettice, Yellow Pepper, Orange Pepper, Tomato, Feta Cheese,, Hot House Cucomber", Picture : "Greek Salad")
+    
+    static let Tossed = Salad (Name : "Tossed Salad", Ingredents :"Lettice,Tomato,Cucomer, Radish,Carrot, Ham,Cheese, Hard Boiled  Egg,", Picture : "Tossed Salad")
+    
+    
+    static let Ceaser = Salad (Name : "ceaser Salal", Ingredents : "Grelled Chikdn, Lettice, Tomato, Parmasn Cheese", Picture : "Ceaser Salad" )
+    
+    static let bean = (Name : "bean Salad", Ingredends : "green bean, gonzbo beans kidney bean, White wine vinger,Diced Red Onions", Picture : "Bean Salad")
+    
+    static let Chopped = Salad (Name : "Chopped Salad", Ingredents : "Iceburg lettace,Tomato, Cucomber, broccili, American Cheese, Ham, Hard boilled egg", Picture : "Chopped Salad")
+    
+    static let banana = Salad (Name : "Banana Salad", Ingredents : "Mixed Greens, Banannas, Peanunts, Creamy Peanut Butter, Mayo, Honey, Lemon Juice", Picture : "Banana Salad")
+    
+    static let Ambrosia = Salad (Name : "Ambrosia Salad", Ingredents : "Pecans,Heavy Cream, Sour Cream, Mini Mashmallows, Diced Pinapples, Shreaded Coconut, Manderian Oranges, Marshino Cherries", Picture :"Ambrosia Salad" )
+
+    static let Seafood = Salad (Name : "Seafood Salad", Ingredents : "Grapefurit, Naval Orange, Olive Oil, Salt, Pepper, Tuna", Picture : "Seafood Salad")
+}
+
+
+
+
